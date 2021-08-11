23922b5eba78a9dc15c181e854facc83baa5ab8f
Richard <richardcgayle@gmail.com>
2021-08-11 15:10:29 -0400

    responsive design update

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 console.log('hi');
 
 const burger = document.querySelector('.burger-menu')
-const navContainer = document.querySelector('.nav-container')
+const standardUl = document.querySelector('.standard-ul')
 const liEl = document.querySelectorAll('.standard-ul li')
 
 burger.addEventListener('click', () => {
-    navContainer.classList.toggle('open')
+    standardUl.classList.toggle('open')
     liEl.forEach(li => {
         li.classList.toggle('fade');
     });
