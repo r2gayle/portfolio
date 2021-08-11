25eace997c0bb7376d10a0ef8025792dcd00634e
Richard <richardcgayle@gmail.com>
2021-08-11 12:48:42 -0400

    mobile nav

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 console.log('hi');
+
+const burger = document.querySelector('.burger-menu')
+const navContainer = document.querySelector('.nav-container')
+const liEl = document.querySelectorAll('.standard-ul li')
+
+burger.addEventListener('click', () => {
+    navContainer.classList.toggle('open')
+    liEl.forEach(li => {
+        li.classList.toggle('fade');
+    });
+});
