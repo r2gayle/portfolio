a58630582dc9f929f97e7ee958ebd8aa499b5244
Richard <richardcgayle@gmail.com>
2021-08-18 20:03:06 -0400

    major JS change

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const burger = document.querySelector('.burger-menu')
 const standardUl = document.querySelector('.standard-ul')
+const nav = document.querySelector('.nav-container')
 const liEl = document.querySelectorAll('.standard-ul li')
 const aLink = document.querySelectorAll('.standard-ul li a')
 const lineOne = document.querySelector('.line-1')
@@ -10,6 +11,7 @@ const lineThree = document.querySelector('.line-3')
 
 burger.addEventListener('click', () => {
     standardUl.classList.toggle('open')
+    nav.classList.toggle('padding')
     lineOne.classList.toggle('color')
     lineTwo.classList.toggle('color')
     lineThree.classList.toggle('color')
@@ -23,6 +25,7 @@ burger.addEventListener('click', () => {
 // To remove nav once a link is clicked
 const closeNav = () => {
     standardUl.classList.remove("open");
+    nav.classList.remove('padding')
     lineOne.classList.toggle('color')
     lineTwo.classList.toggle('color')
     lineThree.classList.toggle('color')
